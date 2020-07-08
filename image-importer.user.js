a6e33e7ab4bf057508b9d0d89e11ee99c10485f7
Mark Taiwan <marktaiwan@users.noreply.github.com>
2020-07-08 14:02:20 +0800

    Generalize script
    Script can now import images from any of the following sites to another:
    - Derpibooru
    - Ponybooru
    - Ponerpics

--- image-importer.user.js
+++ image-importer.user.js
@@ -1,13 +1,29 @@
 // ==UserScript==
 // @name         Derpibooru Image Importer
+// @description  Import image and tags from Philomena-based boorus
 // @version      1.2.5
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
 // @homepageURL  https://github.com/marktaiwan/Philomena-Importer
 // @supportURL   https://github.com/marktaiwan/Philomena-Importer/issues
+//
+// @match        *://*.derpibooru.org/images/new
+// @match        *://*.derpibooru.org/settings/edit
+// @match        *://*.trixiebooru.org/images/new
+// @match        *://*.trixiebooru.org/settings/edit
+//
 // @match        *://*.ponybooru.org/images/new
 // @match        *://*.ponybooru.org/settings/edit
+//
+// @match        *://*.ponerpics.org/images/new
+// @match        *://*.ponerpics.org/settings/edit
+//
+// @connect      derpibooru.org
+// @connect      derpicdn.net
+// @connect      ponybooru.org
+// @connect      ponerpics.org
+//
 // @inject-into  content
 // @grant        GM_xmlhttpRequest
 // @noframes
@@ -17,7 +33,27 @@
 (function () {
 'use strict';
 
-const origDomain = 'https://derpibooru.org';
+const boorus = {
+  derpibooru: {
+    primaryDomain: 'https://derpibooru.org',
+    prettyName: 'Derpibooru',
+    booruDomains: ['derpibooru.org', 'trixiebooru.org'],
+    cdnDomains: ['derpicdn.net'],
+  },
+  ponybooru: {
+    primaryDomain: 'https://ponybooru.org',
+    prettyName: 'Ponybooru',
+    booruDomains: ['ponybooru.org'],
+    cdnDomains: ['cdn.ponybooru.org'],
+  },
+  ponerpics: {
+    primaryDomain: 'https://ponerpics.org',
+    prettyName: 'Ponerpics',
+    booruDomains: ['ponerpics.org'],
+    cdnDomains: ['ponerpics.org'],
+  }
+};
+
 const DEFAULT_TAG_BLACKLIST = [
   'adventure in the comments',
   'changelings in the comments',
@@ -35,25 +71,29 @@ const DEFAULT_TAG_BLACKLIST = [
   'translated in the comments',
 ];
 
-const config = ConfigManager('Derpibooru Image Importer', 'image_importer');
+const config = ConfigManager(
+  'Philomena Image Importer',
+  'image_importer',
+  'Import image and tags from Philomena-based boorus.'
+);
 config.registerSetting({
   title: 'Add import message to description',
   key: 'indicate_import',
-  description: 'Prefix the description with a link to the original derpibooru post.',
+  description: 'Prefix the description with a link to the original post.',
   type: 'checkbox',
   defaultValue: false
 });
 config.registerSetting({
   title: 'Link correction',
   key: 'link_fix',
-  description: 'Rewrite original on-site links in the description to properly point to Derpibooru.',
+  description: 'Rewrite on-site links in the description to properly point to the original site.',
   type: 'checkbox',
   defaultValue: true
 });
 config.registerSetting({
-  title: 'Use Derpi as source',
-  key: 'derpi_source',
-  description: 'Use Derpibooru as source link if the imported image lacks one.',
+  title: 'Use origin as source',
+  key: 'origin_source',
+  description: 'Use the original post as source link if the imported image lacks one.',
   type: 'checkbox',
   defaultValue: false
 });
@@ -77,7 +117,7 @@ const tagEntry = tagFieldset.registerSetting({
 
 const INDICATE_IMPORT = config.getEntry('indicate_import');
 const LINK_FIX = config.getEntry('link_fix');
-const DERPI_SOURCE = config.getEntry('derpi_source');
+const ORIGIN_SOURCE = config.getEntry('origin_source');
 const TAG_FILTER = config.getEntry('tag_filter');
 
 /*
@@ -112,13 +152,16 @@ function $(selector, parent = document) {
   return parent.querySelector(selector);
 }
 
-async function importImage(imageID) {
-  const importButton = $('#derpi_import_button');
+async function importImage(imageID, booruData) {
+  const makeAbsolute = (path, domain) => path.match(/^(?:https?:)?\/\//) ? path : domain + path;
+
+  const {primaryDomain} = booruData;
+  const importButton = $('#script_import_button');
   importButton.innerText = 'Loading...';
 
-  // fetch image metadata from Derpi
-  const json = await makeRequest('https://derpibooru.org/api/v1/json/images/' + imageID).then(resp => JSON.parse(resp.responseText));
-  const fileURL = json.image.representations.full;
+  // fetch image metadata
+  const json = await makeRequest(`${primaryDomain}/api/v1/json/images/` + imageID).then(resp => JSON.parse(resp.responseText));
+  const fileURL = makeAbsolute(json.image.representations.full, primaryDomain);
   const {description, tags, mime_type: mimeType, source_url: source, name: fileName} = json.image;
   const tagInput = $('#image_tag_input');
   const fancyEditor = tagInput.classList.contains('hidden');
@@ -132,8 +175,8 @@ async function importImage(imageID) {
   // add source
   $('#image_source_url').value = (source)
     ? source
-    : (DERPI_SOURCE)
-      ? `${origDomain}/images/${imageID}`
+    : (ORIGIN_SOURCE)
+      ? `${primaryDomain}/images/${imageID}`
       : '';
 
   // add tags
@@ -149,7 +192,7 @@ async function importImage(imageID) {
   tagInput.value = addedTags.join(', ');
 
   // add description
-  $('#image_description').value = processDescription(description, imageID);
+  $('#image_description').value = processDescription(description, imageID, booruData);
 
   // revert tag editor
   if (fancyEditor) {
@@ -181,7 +224,7 @@ function initUI(){
   importButton.setAttribute('class', 'button button--separate-left');
   importButton.type = 'button';
   importButton.innerText = 'Import';
-  importButton.id = 'derpi_import_button';
+  importButton.id = 'script_import_button';
   importButton.style.width = '100px';
   fetchButton.insertAdjacentElement('beforebegin', importButton);
 
@@ -191,30 +234,43 @@ function initUI(){
 
     const input = $('#image_scraper_url');
     const url = input.value.trim();
-    importImage(getImageId(url));
+    try {
+      const {id, booruData} = getImageInfo(url);
+      importImage(id, booruData);
+    } catch (exception) {
+      if (exception.message === 'no_match') {
+        console.error('Failed to match input URL.');
+      } else if (exception.message === 'same_site') {
+        console.error('You can\'t import images from the same site. Moron.');
+      } else {
+        throw exception;
+      }
+    }
   });
 }
 
-function processDescription(original, imageID) {
-  const emptyDesc = (original === '');
-  let desc = original;
+function processDescription(originalDescription, imageID, booruData) {
+  const {primaryDomain, prettyName} = booruData;
+  const emptyDesc = (originalDescription === '');
+  let desc = originalDescription;
 
   if (LINK_FIX && !emptyDesc) {
     // rewrite in-site links
+    // match links begin with "/" but not "//"
     desc = desc.replace(
-      /(".+?"):(\/)/g,
-      (matched, text) => `${text}:${origDomain}/`
+      /(".+?"):(?:\/)(?!\/)/g,
+      (matched, text) => `${text}:${primaryDomain}/`
     );
     // rewrite image links
     // match image links, turn embeds into links as well.
     desc = desc.replace(
       /(?:>>(\d+))[pts]?/g,
-      (matched, id) => `"[==${matched}==]":${origDomain}/images/${id}`
+      (matched, id) => `"[==${matched}==]":${primaryDomain}/images/${id}`
     );
   }
 
   if (INDICATE_IMPORT) {
-    const msg = `"[Imported from Derpibooru]":${origDomain}/images/${imageID}`;
+    const msg = `"[Imported from ${prettyName}]":${primaryDomain}/images/${imageID}`;
     desc = emptyDesc ? msg : msg + '\n\n' + desc;
   }
 
@@ -232,7 +288,7 @@ function makeRequest(url, responseType = 'text', onprogress) {
       responseType,
       onload: resolve,
       onerror: (e) => {
-        $('#derpi_import_button').innerText = 'Error';
+        $('#script_import_button').innerText = 'Error';
         console.log(e);
       },
       onprogress
@@ -243,17 +299,56 @@ function makeRequest(url, responseType = 'text', onprogress) {
 function progressCallback(response) {
   if (!response.lengthComputable || response.readyState !== 3) return;
 
-  const button = $('#derpi_import_button');
+  const button = $('#script_import_button');
   const {loaded, total} = response;
   const percentage = Math.round((loaded / total) * 100);
 
   button.innerText = `${percentage}%`;
 }
 
-function getImageId(url) {
-  const regex = new RegExp('https?://(?:www\\.)?(?:(?:derpibooru\\.org|trixiebooru\\.org)/(?:images/)?(\\d+)(?:\\?.*|/|\\.html)?|derpicdn\\.net/img/(?:view/|download/)?\\d+/\\d+/\\d+/(\\d+))', 'i');
-  const array = url.match(regex);
-  return (array !== null) ? array[1] || array[2] : null;
+function getImageInfo(url) {
+  const domainRegexStr = concatDomains(boorus, 'booruDomains');
+  const cdnRegexStr = concatDomains(boorus, 'cdnDomains');
+  const regex = new RegExp(
+    'https?://(?:www\\.)?(?:' +
+      `(?<domain>${domainRegexStr})/(?:images/)?(?<domID>\\d+)(?:\\?.*|/|\\.html)?|` +
+      `(?<cdn>${cdnRegexStr})/img/(?:view/|download/)?\\d+/\\d+/\\d+/(?<cdnID>\\d+)` +
+    ')', 'i');
+
+  const result = regex.exec(url);
+  if (result === null) {
+    throw Error('no_match');
+  }
+
+  const {domain, cdn, domID, cdnID} = result.groups;
+  const matchedDomain = domain || cdn;
+  const booruData = getDomainInfo(matchedDomain);
+  const id = domID || cdnID;
+
+  return {id, booruData};
+}
+
+function concatDomains(boorus, prop) {
+  const arr = [];
+  for (const booru of Object.values(boorus)) {
+    for (const domain of booru[prop]) {
+      const regexStr = domain.replace(/\./g, '\\.');
+      arr.push(regexStr);
+    }
+  }
+  return arr.join('|');
+}
+
+function getDomainInfo(domain) {
+  for (const booru of Object.values(boorus)) {
+    const validDomains = [...booru.booruDomains, ...booru.cdnDomains];
+    if (validDomains.includes(domain)) {
+      if (validDomains.includes(window.location.host)) {
+        throw Error('same_site');
+      }
+      return booru;
+    }
+  }
 }
 
 initUI();
