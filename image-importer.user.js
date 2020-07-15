a884f9745891935fc4e147924f929220bff66ab5
Mark Taiwan <marktaiwan@users.noreply.github.com>
2020-07-15 17:54:35 +0800

    Version bump 1.4.5

--- image-importer.user.js
+++ image-importer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Derpibooru Image Importer
 // @description  Import image and tags from Philomena-based boorus
-// @version      1.4.4
+// @version      1.4.5
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
